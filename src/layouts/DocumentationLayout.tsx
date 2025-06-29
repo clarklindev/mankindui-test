54f65b97ff38b29d90eb75ff562a1f410af0f238
clarklindev <clarklindev@gmail.com>
2025-06-29 11:33:45 +0800

    remove redundant code

--- src/layouts/DocumentationLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function DocumentationLayout() {
-  return (
-    <div>
-      <Outlet /> {/* This is where child routes render */}
-    </div>
-  );
-}
